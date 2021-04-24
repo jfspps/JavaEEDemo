627abe61e5e7126ad016c60422fde0b0088bc50a
jfspps <jfsppsDev@gmail.com>
2021-04-24 19:18:02 +0100

    added REST controller/servlet

--- src/main/java/service/TodoService.java
+++ src/main/java/service/TodoService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -12,10 +14,21 @@ public class TodoService {
 	@PersistenceContext
 	EntityManager entityManager;
 	
-	public Todo createTodp(Todo todo) {
+	public Todo createTodo(Todo todo) {
 		entityManager.persist(todo);
 		return todo;
 	}
 	
+	public Todo updateTodo(Todo todo) {
+		entityManager.merge(todo);
+		return todo;
+	}
 	
+	public Todo findTodoById(Long id) {
+		return entityManager.find(Todo.class, id);
+	}
+	
+	public List<Todo> getTodos(){
+		return entityManager.createQuery("SELECT t FROM Todo t", Todo.class).getResultList();
+	}
 }
